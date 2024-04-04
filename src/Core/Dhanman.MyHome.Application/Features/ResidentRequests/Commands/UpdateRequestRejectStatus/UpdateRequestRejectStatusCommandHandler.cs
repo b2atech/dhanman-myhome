62f9979837e748bdeccd4e46e5875fbb65152ded
Bhalchandra <bvsahasrabuddhe@gmail.com>
2024-04-05 00:44:58 +0530

    feat: update status approved and rejected

---
+++ src/Core/Dhanman.MyHome.Application/Features/ResidentRequests/Commands/UpdateRequestRejectStatus/UpdateRequestRejectStatusCommandHandler.cs
@@ -0,0 +1,46 @@
+﻿using B2aTech.CrossCuttingConcern.Core.Result;
+using Dhanman.MyHome.Application.Abstractions.Messaging;
+using Dhanman.MyHome.Application.Contracts.Common;
+using Dhanman.MyHome.Application.Features.ResidentRequests.Events;
+using Dhanman.MyHome.Domain.Abstractions;
+using Dhanman.MyHome.Domain.Exceptions;
+using MediatR;
+
+namespace Dhanman.MyHome.Application.Features.ResidentRequests.Commands.UpdateRequestRejectStatus;
+
+public class UpdateRequestRejectStatusCommandHandler : ICommandHandler<UpdateRequestRejectStatusCommand, Result<EntityUpdatedResponse>>
+{
+    #region Properties
+    private readonly IResidentRequestRepository _residentRequestRepository;
+    private readonly IMediator _mediator;
+    #endregion
+
+    #region Constructors
+    public UpdateRequestRejectStatusCommandHandler(IResidentRequestRepository residentRequestRepository, IMediator mediator)
+    {
+        _residentRequestRepository = residentRequestRepository;
+        _mediator = mediator;
+    }
+    #endregion
+
+    #region Methods
+    public async Task<Result<EntityUpdatedResponse>> Handle(UpdateRequestRejectStatusCommand request, CancellationToken cancellationToken)
+    {
+        var updateRequestRejectStatus = await _residentRequestRepository.GetBydIdIntAsync(request.Id);
+
+        if (updateRequestRejectStatus is null)
+        {
+            throw new RequestIdNotFoundException(request.Id);
+        }
+
+        updateRequestRejectStatus.RequestStatusId = 3;
+
+        _residentRequestRepository.Update(updateRequestRejectStatus);
+
+        await _mediator.Publish(new ResidentRequestUpdatedEvent(updateRequestRejectStatus.Id), cancellationToken);
+        return Result.Success(new EntityUpdatedResponse(updateRequestRejectStatus.Id));
+    }
+
+    #endregion
+
+}
