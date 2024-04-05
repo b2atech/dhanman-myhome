2c8618856acec0b5b4b769bdd8047870bcd84ff0
Ritika B <ritikabhusannavar28@gmail.com>
2024-04-05 15:03:10 +0530

    feat: error handled

--- src/Core/Dhanman.MyHome.Application.Contracts/Units/CreateUnitRequest.cs
+++ src/Core/Dhanman.MyHome.Application.Contracts/Units/CreateUnitRequest.cs
@@ -9,7 +9,7 @@ public class CreateUnitRequest
     public string Occupant { get; set; }
     public string Occupancy { get; set; }
     public string PrimaryEIntercom { get; set; }
-    public string eIntercom { get; set; }
+    public string EIntercom { get; set; }
     public string Latitude { get; set; }
     public string Longitude { get; set; }
     public int NumberOfMembers { get; set; }
@@ -19,29 +19,20 @@ public class CreateUnitRequest
     #endregion
 
     #region Constructor
-    public CreateUnitRequest(CreateUnitRequestParams parameters)
+    public CreateUnitRequest(string status, string flat, string flatType, string occupant, string occupancy, string primaryEIntercom, string eIntercom, string latitude, string longitude, int numberOfMembers, string society, Guid createdBy)
     {
-        Status = parameters.Status;
-        Flat = parameters.Flat;
-        FlatType = parameters.FlatType;
-        Occupant = parameters.Occupant;
-        Occupancy = parameters.Occupancy;
-        PrimaryEIntercom = parameters.PrimaryEIntercom;
-        NumberOfMembers = parameters.NumberOfMembers;
-        Society = parameters.Society;
-        CreatedBy = parameters.CreatedBy;
+        Status = status;
+        Flat = flat;
+        FlatType = flatType;
+        Occupant = occupant;
+        Occupancy = occupancy;
+        PrimaryEIntercom = primaryEIntercom;
+        EIntercom = eIntercom;
+        Latitude = latitude;
+        Longitude = longitude;
+        NumberOfMembers = numberOfMembers;
+        Society = society;
+        CreatedBy = createdBy;
     }
     #endregion
-}
-public class CreateUnitRequestParams
-{
-    public string Status { get; set; }
-    public string Flat { get; set; }
-    public string FlatType { get; set; }
-    public string Occupant { get; set; }
-    public string Occupancy { get; set; }
-    public string PrimaryEIntercom { get; set; }
-    public int NumberOfMembers { get; set; }
-    public string Society { get; set; }
-    public Guid CreatedBy { get; set; }
 }
