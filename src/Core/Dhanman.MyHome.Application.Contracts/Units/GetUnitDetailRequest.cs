47d7f11cfa8f9d62119c0511758cdf194a8a6333
shreyaskhamkar <shreyaskhamkar96@gmail.com>
2024-05-07 12:19:28 +0530

    refactor unitdetail endpint #195

---
+++ src/Core/Dhanman.MyHome.Application.Contracts/Units/GetUnitDetailRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace Dhanman.MyHome.Application.Contracts.Units;
+
+public sealed class GetUnitDetailRequest
+{
+    public List<int> BuidingIds { get; set; }
+    public List<int> OccupancyIds { get; set; }
+
+    public GetUnitDetailRequest(List<int> buidingIds, List<int> occupancyIds)
+    {
+       BuidingIds = buidingIds;
+       OccupancyIds = occupancyIds;
+    }
+ 
+}
