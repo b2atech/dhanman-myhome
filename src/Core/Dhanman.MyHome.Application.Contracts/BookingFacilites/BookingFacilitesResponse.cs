a4ae5c95ed7c1f32ae2bfe3948fa70659fc656d0
shreyaskhamkar <shreyaskhamkar96@gmail.com>
2024-05-09 15:29:57 +0530

    remove unsused properties from response

--- src/Core/Dhanman.MyHome.Application.Contracts/BookingFacilites/BookingFacilitesResponse.cs
+++ src/Core/Dhanman.MyHome.Application.Contracts/BookingFacilites/BookingFacilitesResponse.cs
@@ -6,12 +6,6 @@ public class BookingFacilitesResponse
     public int Id { get; }
     public string Name { get; }
     public int BuildingId { get; }
-    public DateTime CreatedOnUtc { get; }
-    public DateTime? ModifiedOnUtc { get; }
-    public DateTime? DeletedOnUtc { get; }
-    public bool IsDeleted { get; }
-    public Guid CreatedBy { get; }
-    public Guid? ModifiedBy { get; }
 
     public BookingFacilitesResponse(int id, string name, int buildingId)
     {
