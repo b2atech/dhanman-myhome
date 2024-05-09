3fe69a8d38a3fd52e26b88860e5de339f262e15b
Bhalchandra <bvsahasrabuddhe@gmail.com>
2024-05-09 19:07:21 +0530

    feat: merging controller

--- src/Dhanman.MyHome.Api/Controllers/ServiceProvidersController.cs
+++ src/Dhanman.MyHome.Api/Controllers/ServiceProvidersController.cs
@@ -2,9 +2,9 @@
 using Dhanman.MyHome.Api.Contracts;
 using Dhanman.MyHome.Api.Infrastructure;
 using Dhanman.MyHome.Application.Contracts.ServiceProviders;
-using Dhanman.MyHome.Application.Contracts.Vehicles;
+using Dhanman.MyHome.Application.Contracts.ServiceProviderSubTypes;
 using Dhanman.MyHome.Application.Features.ServiceProviders.Queries;
-using Dhanman.MyHome.Application.Features.Vehicles.Queries;
+using Dhanman.MyHome.Application.Features.ServiceProviderSubType.Queries;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -38,5 +38,16 @@ public class ServiceProvidersController : ApiController
     #endregion
 
 
+    #region ServiceProviderSubType     
 
+    [HttpGet(ApiRoutes.ServiceProviderSubType.GetAllServiceProvderSubType)]
+    [ProducesResponseType(typeof(ServiceProivderSubTypeResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetAllUnits() =>
+    await Result.Success(new GetAllServiceProviderSubTypeQuery())
+    .Bind(query => Mediator.Send(query))
+    .Match(Ok, NotFound);
+
+
+    #endregion
 }
