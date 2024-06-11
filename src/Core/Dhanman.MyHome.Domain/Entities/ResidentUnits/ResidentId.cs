28ffdf429354150a48b1af163889a8cc22e1a45f
shreyaskhamkar <shreyaskhamkar96@gmail.com>
2024-06-11 09:56:52 +0530

    fix: resident and UnitId class

--- src/Core/Dhanman.MyHome.Domain/Entities/ResidentUnits/ResidentId.cs
+++ src/Core/Dhanman.MyHome.Domain/Entities/ResidentUnits/ResidentId.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Dhanman.MyHome.Domain.Entities.ResidentUnits;
 
-namespace Dhanman.MyHome.Domain.Entities.ResidentUnits
-{
-    internal class ResidentId
-    {
-    }
-}
+
+public record ResidentId(int Value);
--- src/Core/Dhanman.MyHome.Domain/Entities/ResidentUnits/UnitId.cs
+++ src/Core/Dhanman.MyHome.Domain/Entities/ResidentUnits/UnitId.cs
@@ -1,12 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Dhanman.MyHome.Domain.Entities.ResidentUnits;
 
-namespace Dhanman.MyHome.Domain.Entities.ResidentUnits
-{
-    internal class UnitId
-    {
-    }
-}
+public record UnitId(int Value);
