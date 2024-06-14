2e1b829c7ab164cc95ddda47e825948af3a0e348
shreyaskhamkar <shreyaskhamkar96@gmail.com>
2024-06-14 16:34:01 +0530

    feat: get all units by floor id endpoint

---
+++ src/Core/Dhanman.MyHome.Application.Contracts/Units/GetUnitsByFloorIdRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace Dhanman.MyHome.Application.Contracts.Units;
+
+public sealed class GetUnitsByFloorIdRequest
+{
+    public List<int> BuildingIds { get; set; }
+    public List<int> FloorIds { get; set; }
+
+    public GetUnitsByFloorIdRequest(List<int> buildingIds, List<int> floorIds)
+    {
+        BuildingIds = buildingIds;
+        FloorIds = floorIds;
+    }
+}
