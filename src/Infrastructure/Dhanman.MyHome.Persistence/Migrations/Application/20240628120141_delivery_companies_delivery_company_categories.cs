8b3bfb5f976d64b81e7218f25cf15f68dcd3be6d
Bhalchandra <bvsahasrabuddhe@gmail.com>
2024-06-28 17:58:09 +0530

    feat: migration

---
+++ src/Infrastructure/Dhanman.MyHome.Persistence/Migrations/Application/20240628120141_delivery_companies_delivery_company_categories.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Dhanman.MyHome.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class delivery_companies_delivery_company_categories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "delivery_companies",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    delivery_company_category_id = table.Column<int>(type: "integer", nullable: false),
+                    icon = table.Column<string>(type: "text", nullable: false),
+                    created_on_utc = table.Column<DateTime>(type: "timestamp", nullable: false),
+                    modified_on_utc = table.Column<DateTime>(type: "timestamp", nullable: true),
+                    deleted_on_utc = table.Column<DateTime>(type: "timestamp", nullable: true),
+                    is_deleted = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false),
+                    created_by = table.Column<Guid>(type: "uuid", nullable: false),
+                    modified_by = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_delivery_companies", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "delivery_company_categories",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    deleted_on_utc = table.Column<DateTime>(type: "timestamp", nullable: true),
+                    is_deleted = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_delivery_company_categories", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "delivery_companies");
+
+            migrationBuilder.DropTable(
+                name: "delivery_company_categories");
+        }
+    }
+}
