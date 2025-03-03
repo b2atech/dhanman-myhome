8fcac6102c091b131382a308dee036c189b23a0e
shreyaskhamkar <shreyaskhamkar96@gmail.com>
2025-03-03 14:43:25 +0530

    feat: create get all IdentityTypes endpoint

---
+++ src/Core/Dhanman.MyHome.Application.Contracts/IdendityTypes/IdentityTypeListResponse.cs
@@ -0,0 +1,19 @@
+﻿namespace Dhanman.MyHome.Application.Contracts.IdendityTypes;
+
+public sealed class IdentityTypeListResponse
+{
+    #region Properties 
+
+    public string Cursor { get; }
+    public IReadOnlyCollection<IdendityTypeResponse> Items { get; }
+    #endregion
+
+    #region Constructor
+
+    public IdentityTypeListResponse(IReadOnlyCollection<IdendityTypeResponse> items, string cursor = "")
+    {
+        Items = items;
+        Cursor = cursor;
+    }
+    #endregion
+}
