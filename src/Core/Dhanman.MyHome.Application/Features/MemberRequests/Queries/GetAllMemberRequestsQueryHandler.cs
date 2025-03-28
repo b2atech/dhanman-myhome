6fff4339d5bd29af26d0e76e95f6d1ec2a9c5729
Bhalchandra <bvsahasrabuddhe@gmail.com>
2025-03-28 20:58:16 +0530

    refactor: get member request

--- src/Core/Dhanman.MyHome.Application/Features/MemberRequests/Queries/GetAllMemberRequestsQueryHandler.cs
+++ src/Core/Dhanman.MyHome.Application/Features/MemberRequests/Queries/GetAllMemberRequestsQueryHandler.cs
@@ -4,6 +4,8 @@ using Dhanman.MyHome.Application.Abstractions.Messaging;
 using Dhanman.MyHome.Application.Contracts.MemberRequests;
 using Dhanman.MyHome.Domain;
 using Dhanman.MyHome.Domain.Entities.Apartments;
+using Dhanman.MyHome.Domain.Entities.Cities;
+using Dhanman.MyHome.Domain.Entities.MemberAdditionalDetails;
 using Dhanman.MyHome.Domain.Entities.ResidentRequests;
 using Dhanman.MyHome.Domain.Entities.ResidentStatuses;
 using Microsoft.EntityFrameworkCore;
@@ -21,38 +23,52 @@ public class GetAllMemberRequestsQueryHandler : IQueryHandler<GetAllMemberReques
     #endregion
 
     #region Methods
-
     public async Task<Result<MemberRequestListResponse>> Handle(GetAllMemberRequestsQuery request, CancellationToken cancellationToken)
     {
-        return await Result.Success(request)
-              .Ensure(query => query != null, Errors.General.EntityNotFound)
-              .Bind(async query =>
-              {
-                  var memberRequests = await _dbContext.SetInt<ResidentRequest>()
-                  .AsNoTracking()
-                  .Where(e => e.ApartmentId == request.ApartmentId)
-                  .Select(e => new MemberRequestResponse(
-                          e.Id,
-                          e.ApartmentId,
-                          _dbContext.Set<Apartment>()
-                                  .Where(p => p.Id == e.ApartmentId)
-                                  .Select(p => p.Name).FirstOrDefault(),                          
-                          e.FirstName,
-                          e.LastName,                          
-                          e.Email,
-                          e.ContactNumber,                         
-                          e.RequestStatusId,
-                          _dbContext.SetInt<ResidentRequestStatus>()
-                                  .Where(p => p.Id == e.RequestStatusId)
-                                  .Select(p => p.Name).FirstOrDefault()                         
-                          ))
-                  .ToListAsync(cancellationToken);
+        if (request == null)
+            return Result.Failure<MemberRequestListResponse>(Errors.General.EntityNotFound);
 
-                  var listResponse = new MemberRequestListResponse(memberRequests);
+        var memberRequests = await (from e in _dbContext.SetInt<ResidentRequest>().AsNoTracking()
+                                    join a in _dbContext.Set<Apartment>().AsNoTracking() on e.ApartmentId equals a.Id into apt
+                                    from a in apt.DefaultIfEmpty()
+                                    join m in _dbContext.Set<MemberAdditionalDetail>().AsNoTracking() on e.MemberAdditionalDetailsId equals m.Id into mad
+                                    from m in mad.DefaultIfEmpty()
+                                    join r in _dbContext.SetInt<ResidentRequestStatus>().AsNoTracking() on e.RequestStatusId equals r.Id into rs
+                                    from r in rs.DefaultIfEmpty()
+                                    join city in _dbContext.Set<City>().AsNoTracking() on m.HattyId equals city.Id into cityGroup
+                                    from city in cityGroup.DefaultIfEmpty()
+                                    join spouseCity in _dbContext.Set<City>().AsNoTracking() on m.SpouseHattyId equals spouseCity.Id into spouseCityGroup
+                                    from spouseCity in spouseCityGroup.DefaultIfEmpty()
+                                    where e.ApartmentId == request.ApartmentId
+                                    select new MemberRequestResponse(
+                                        e.Id,
+                                        e.ApartmentId,
+                                        a.Name,
+                                        e.FirstName,
+                                        e.LastName,
+                                        e.Email,
+                                        e.ContactNumber,
+                                        e.MemberAdditionalDetailsId,
+                                        m.MemberType,
+                                        m.UserName,
+                                        m.Password,
+                                        m.HattyId,
+                                        city.Name,
+                                        m.CompanyName,
+                                        m.Designation,
+                                        m.DateOfBirth,
+                                        m.Gender,
+                                        m.MaritalStatus,
+                                        m.AboutYourself,
+                                        m.SpouseName,
+                                        m.SpouseHattyId,
+                                        spouseCity.Name,
+                                        e.RequestStatusId,
+                                        r.Name
+                                    )).ToListAsync(cancellationToken);
 
-                  return listResponse;
-              });
-    }
+        return Result.Success(new MemberRequestListResponse(memberRequests));
+    }    
     #endregion
 
 }
