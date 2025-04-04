c94e163a072ab0b6f61c59375c58c6f2d5a86dd9
Ritika B <ritikabhusannavar28@gmail.com>
2025-04-04 12:04:33 +0530

    feat: sonar issues

--- src/Core/Dhanman.MyHome.Application/Features/Visitors/Commands/CreateVisitorAndLog/CreateVisitorPendingCommandHandler.cs
+++ src/Core/Dhanman.MyHome.Application/Features/Visitors/Commands/CreateVisitorAndLog/CreateVisitorPendingCommandHandler.cs
@@ -61,32 +61,6 @@ public class CreateVisitorPendingCommandHandler : ICommandHandler<CreateVisitorP
         return Result.Success(new EntityCreatedResponse(visitorId));
     }
 
-
-    //public async Task<Result<EntityCreatedResponse>> Handle(CreateVisitorPendingCommand request, CancellationToken cancellationToken)
-    //{
-    //    var parameters = new[]
-    //    {
-    //        new NpgsqlParameter("p_apartment_id", NpgsqlDbType.Uuid) { Value = request.ApartmentId },
-    //        new NpgsqlParameter("p_first_name", NpgsqlDbType.Text) { Value = request.FirstName },
-    //        new NpgsqlParameter("p_last_name", NpgsqlDbType.Text) { Value = request.LastName },
-    //        new NpgsqlParameter("p_email", NpgsqlDbType.Text) { Value = request.Email },
-    //        new NpgsqlParameter("p_contact_number", NpgsqlDbType.Text) { Value = request.ContactNumber },
-    //        new NpgsqlParameter("p_vehicle_number", NpgsqlDbType.Text) { Value = request.VehicleNumber },
-    //        new NpgsqlParameter("p_identity_type_id", NpgsqlDbType.Integer) { Value = request.IdentityTypeId },
-    //        new NpgsqlParameter("p_identity_number", NpgsqlDbType.Text) { Value = request.IdentityNumber },
-    //        new NpgsqlParameter("p_visitor_type_id", NpgsqlDbType.Integer) { Value = request.VisitorTypeId },
-    //        new NpgsqlParameter("p_visiting_from", NpgsqlDbType.Text) { Value = request.VisitingFrom },
-    //        new NpgsqlParameter("p_created_by", NpgsqlDbType.Uuid) { Value = request.CreatedBy }
-    //    };
-
-    //    await _dbContext.Database.ExecuteSqlRawAsync(
-    //        "select * from public.save_visitor_and_pending(@p_apartment_id, @p_first_name, @p_last_name, @p_email, @p_contact_number, @p_vehicle_number, @p_identity_type_id, @p_identity_number, @p_visitor_type_id, @p_visiting_from, @p_created_by)",
-    //        parameters
-    //    );
-
-    //    await _mediator.Publish(new VisitorCreatedEvent(request.VisitorId), cancellationToken);
-    //    return Result.Success(new EntityCreatedResponse(request.VisitorId));
-    //}
     #endregion
 }
 
