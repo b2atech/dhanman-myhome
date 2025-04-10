6a7b1426c8c49c15c1418df5e7b5bdc38d808349
karankhamkar <karankhamkarr@gmail.com>
2025-04-10 18:42:04 +0530

    Feat: create handler

---
+++ src/Core/Dhanman.MyHome.Application/Features/Vehicles/commands/create/CreateVisitorVehicleCommandHandler.cs
@@ -0,0 +1,49 @@
+﻿using B2aTech.CrossCuttingConcern.Core.Result;
+using Dhanman.MyHome.Application.Abstractions.Data;
+using Dhanman.MyHome.Application.Abstractions.Messaging;
+using Dhanman.MyHome.Application.Contracts.Common;
+using Dhanman.MyHome.Application.Features.Vehicles.commands.create;
+using Dhanman.MyHome.Application.Features.Vehicles.Events;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
+using NpgsqlTypes;
+
+namespace Dhanman.MyHome.Application.Features.Vehicles.Commands.Create
+{
+    public class CreateVisitorVehicleCommandHandler : ICommandHandler<CreateVisitorVehicleCommand, Result<EntityCreatedResponse>>
+    {
+        #region Properties
+        private readonly IApplicationDbContext _dbContext;
+        private readonly IMediator _mediator;
+        #endregion
+
+        #region Constructor
+        public CreateVisitorVehicleCommandHandler(IApplicationDbContext dbContext, IMediator mediator)
+        {
+            _dbContext = dbContext;
+            _mediator = mediator;
+        }
+        #endregion
+
+        #region Methods
+        public async Task<Result<EntityCreatedResponse>> Handle(CreateVisitorVehicleCommand request, CancellationToken cancellationToken)
+        {
+            var parameters = new[]
+            {
+                new NpgsqlParameter("p_visitor_log_id", NpgsqlDbType.Integer) { Value = request.VisitorLogId },
+                new NpgsqlParameter("p_vehicle_number", NpgsqlDbType.Text) { Value = string.IsNullOrEmpty(request.VehicleNumber) ? DBNull.Value : (object)request.VehicleNumber },
+                new NpgsqlParameter("P_vehicle_type", NpgsqlDbType.Text) { Value = string.IsNullOrEmpty(request.VehicleType) ? DBNull.Value : (object)request.VehicleType }
+            };
+
+            await _dbContext.Database.ExecuteSqlRawAsync(
+                "CALL public.insert_visitor_vehicle(@p_visitor_log_id, @p_vehicle_number, @P_vehicle_type);",
+                parameters
+            );
+
+            await _mediator.Publish(new VisitorVehicleCreatedEvent(request.VisitorVehicleId), cancellationToken);
+            return Result.Success(new EntityCreatedResponse(request.VisitorVehicleId));
+        }
+        #endregion
+    }
+}
--- src/Dhanman.MyHome.Api/Controllers/VehiclesController.cs
+++ src/Dhanman.MyHome.Api/Controllers/VehiclesController.cs
@@ -2,8 +2,13 @@
 using B2aTech.CrossCuttingConcern.Core.Result;
 using Dhanman.MyHome.Api.Contracts;
 using Dhanman.MyHome.Api.Infrastructure;
+using Dhanman.MyHome.Application.Contracts.Common;
 using Dhanman.MyHome.Application.Contracts.Vehicles;
+using Dhanman.MyHome.Application.Contracts.Visitors;
+using Dhanman.MyHome.Application.Features.Vehicles.commands.create;
 using Dhanman.MyHome.Application.Features.Vehicles.Queries;
+using Dhanman.MyHome.Application.Features.Visitors.Commands.CreateVisitor;
+using Dhanman.MyHome.Domain;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -36,6 +41,20 @@ public class VehiclesController : ApiController
 
     #endregion
 
+    #region Visitor Vehicle
 
+    [HttpPost(ApiRoutes.Vehicles.CreateVisitorVehicle)]
+    [ProducesResponseType(typeof(EntityCreatedResponse), StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> CreateVisitor([FromBody] CreateVisitorVehicleRequest? request) =>
+     await Result.Create(request, Errors.General.BadRequest)
+         .Map(value => new CreateVisitorVehicleCommand(
+             value.VisitorLogId,
+             value.VehicleNumber,
+             value.VehicleType
+         ))
+         .Bind(command => Mediator.Send(command))
+         .Match(Ok, BadRequest);
+    #endregion
 
 }
