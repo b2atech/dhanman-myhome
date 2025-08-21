83dbf44c748839af513afcdcd55432679eb6c9ee
shreyaskhamkar <shreyaskhamkar96@gmail.com>
2025-08-21 20:01:22 +0530

    feat: add the function for GetUnitIdByUserIdendpoint and add the username and unit name

---
+++ src/Core/Dhanman.MyHome.Application.Contracts/Units/BasicUnitInfoResponse.cs
@@ -0,0 +1,20 @@
+﻿namespace Dhanman.MyHome.Application.Contracts.Units;
+
+public class BasicUnitInfoResponse
+{
+    #region Properties 
+
+    public BasicUnitInfo Item { get; }
+    public string Cursor { get; }
+
+    #endregion
+
+    #region Constructor
+
+    public BasicUnitInfoResponse(BasicUnitInfo items, string cursor = "")
+    {
+        Item = items;
+        Cursor = cursor;
+    }
+    #endregion
+}
